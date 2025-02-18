8a8879c554586d9072efbef4cda628255265d880
Roman <roman-meinl@ukr.net>
2025-02-18 13:19:59 -0200

    html , css

---
+++ main.js
@@ -0,0 +1,16 @@
+
+const names = ["Artem","Greg","Andrey","Sergey","Sofia","Sveta","Angelika","Egor","Valeriy"];
+const job = ["Doctor","Driver","Writer","Scientist","Datasataist","Frontender","Designer","Trainer"];
+const abilitys = ["Superman","Batman","Spike","Pluto","Deadpull","Woodywood","Android","MetallBand","Play Guitar"];
+
+function getRandomArr(arr){
+    return arr[Math.floor(Math.random() * arr.lenght)]
+}
+
+const $generate = document.getElementById("generate");
+$generate.addEventListener("click", () => {
+      
+    const character = {
+        
+    }
+})
